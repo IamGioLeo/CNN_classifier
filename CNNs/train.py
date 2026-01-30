36cd9404c6eef1214eef11fc13890259701c5040
Leo <gioleo999@gmail.com>
2026-01-30 17:14:43 +0100

    feat(train): change from pyplot to seaborn to print graphs, add for cycles to explore more parameters

--- CNNs/train.py
+++ CNNs/train.py
@@ -8,7 +8,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
-
+import seaborn as sns
 
 from shallow_net import ShallowCNN
 
@@ -16,11 +16,31 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
 BATCH_SIZE = 32
-NUM_EPOCHS = 40
-LEARNING_RATES = [0.01, 0.001, 0.0001] # To be set...
+NUM_EPOCHS = [24, 40, 60]
+LEARNING_RATES = [0.01, 0.001, 0.0001]
 MOMENTUMS = [0.5, 0.7, 0.9]
-PATIENCE = 5
-OUTPUT_FILE = "shallow_net_v_01_results.txt"
+PATIENCES = [2, 3, 5]
+OUTPUT_FILE = "shallow_net_v_03_results.txt"
+class_codes = [
+    "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14"
+]
+label_map = {
+    "00": "bedroom",
+    "01": "suburb",
+    "02": "industrial",
+    "03": "kitchen",
+    "04": "living_room",
+    "05": "coast",
+    "06": "forest",
+    "07": "highway",
+    "08": "inside_city",
+    "09": "mountain",
+    "10": "open country",
+    "11": "street",
+    "12": "tall building",
+    "13": "office",
+    "14": "store"
+}
 
 ## this is for not correct size images
 #transform = transforms.Compose([
@@ -98,183 +118,262 @@ val_loader = DataLoader(
 test_loader = DataLoader(
     test_set, batch_size=BATCH_SIZE, shuffle=True
 )
+
+sns.set_theme(
+    style="whitegrid",
+    context="talk",
+    font_scale=0.9
+)
+
+
 with open(OUTPUT_FILE, "a") as f:
     f.write("\r\n\nNEW TRIANING SESSION")
 print("\r\n\nNEW TRIANING SESSION")
 for lr in LEARNING_RATES:
     for momentum in MOMENTUMS:
-        with open(OUTPUT_FILE, "a") as f:
-            f.write("\r\n" + "-"*100)
-            f.write(f"\r\nTraining with LEARNING_RATE={lr}, MOMENTUM={momentum}")
-        print("\n" + "-"*100)
-        print(f"Training with LEARNING_RATE={lr}, MOMENTUM={momentum}")
-
-        model = ShallowCNN().to(device)
-        criterion = nn.CrossEntropyLoss()
-
-        optimizer = optim.SGD(
-            model.parameters(),
-            lr=lr,
-            momentum=momentum
-        )
-
-        ## next try 
-        #optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
-        train_losses, val_losses = [], []
-        train_accs, val_accs = [], []
-        global_correct = 0
-        global_total = 0
-
-        best_val_acc = 0
-        best_val_loss = float('inf')
-        epochs_no_improve = 0
-
-        for epoch in range(NUM_EPOCHS):
-            model.train()
-            running_loss = 0.0
-            correct = 0
-            total = 0
-
-            for images, labels in train_loader:
-                images, labels = images.to(device), labels.to(device)
-
-                optimizer.zero_grad()
-                outputs = model(images)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
-
-                running_loss += loss.item()
-                _, predicted = torch.max(outputs, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-
-            train_loss = running_loss / len(train_loader)
-            train_acc = correct / total * 100
-
-            train_losses.append(train_loss)
-            train_accs.append(train_acc)
-
-            model.eval()
-            running_loss = 0.0
-            correct = 0
-            total = 0
-
-            with torch.no_grad():
-                for images, labels in val_loader:
-                    images, labels = images.to(device), labels.to(device)
-
-                    outputs = model(images)
-                    loss = criterion(outputs, labels)
-
-                    running_loss += loss.item()
-                    _, predicted = torch.max(outputs, 1)
-                    total += labels.size(0)
-                    correct += (predicted == labels).sum().item()
-
-                    global_correct += (predicted == labels).sum().item()
-                    global_total += labels.size(0)
-
-            val_loss = running_loss / len(val_loader)
-            val_acc = correct / total * 100
-            val_losses.append(val_loss)
-            val_accs.append(val_acc)
-            with open(OUTPUT_FILE, "a") as f:
-                for line in [
-                    f"\r\nEpoch [{epoch+1}/{NUM_EPOCHS}] | "
-                    f"Train Loss: {train_loss:.4f} | Train Acc: {train_acc:.2f}% | "
-                    f"Val Loss {val_loss:.4f} | Val Acc: {val_acc:.2f}%"
-                ]:
-                    f.write(line)
-            print(
-                f"Epoch [{epoch+1}/{NUM_EPOCHS}] | "
-                f"Train Loss: {train_loss:.4f} | Train Acc: {train_acc:.2f}% | "
-                f"Val Loss {val_loss:.4f} | Val Acc: {val_acc:.2f}%"
-            )
-            
-            if val_acc > best_val_acc:
-                best_val_acc = val_acc
-                torch.save(model.state_dict(), "best_model.pt")
-            
-            if val_loss < best_val_loss:
-                best_val_loss = val_loss
-                epochs_no_improve = 0
-            else:
-                epochs_no_improve += 1
-            
-            if epochs_no_improve >= PATIENCE:
+        for epochs in NUM_EPOCHS:
+            for patience in PATIENCES:
                 with open(OUTPUT_FILE, "a") as f:
-                    f.write("\r\nEarly stopping triggered\n")
-                print("Early stopping triggered")
-                break
-                         
-        model.load_state_dict(torch.load("best_model.pt", map_location=device))
-        model.eval()
+                    f.write("\r\n" + "-"*100)
+                    f.write(f"\r\nTraining with LEARNING_RATE={lr}, MOMENTUM={momentum}, EPOCHS={epochs}, PATIENCE={patience}")
+                print("\n" + "-"*100)
+                print(f"Training with LEARNING_RATE={lr}, MOMENTUM={momentum}, EPOCHS={epochs}, PATIENCE={patience}")
 
-        all_preds = []
-        all_labels = []
+                model = ShallowCNN().to(device)
+                criterion = nn.CrossEntropyLoss()
 
-        with torch.no_grad():
-            for images, labels in test_loader:
-                images = images.to(device)
-                labels = labels.to(device)
+                optimizer = optim.SGD(
+                    model.parameters(),
+                    lr=lr,
+                    momentum=momentum
+                )
 
-                outputs = model(images)
-                _, predicted = torch.max(outputs, 1)
+                ## next try 
+                #optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-                all_preds.append(predicted.cpu())
-                all_labels.append(labels.cpu())
+                train_losses, val_losses = [], []
+                train_accs, val_accs = [], []
+                global_correct = 0
+                global_total = 0
 
-        all_preds = torch.cat(all_preds).numpy()
-        all_labels = torch.cat(all_labels).numpy()
+                best_val_acc = 0
+                best_val_loss = float('inf')
+                epochs_no_improve = 0
+                patience_trigger = False
+                best_val_acc_epoch = 0
+                for epoch in range(epochs):
+                    model.train()
+                    running_loss = 0.0
+                    correct = 0
+                    total = 0
 
-        test_accuracy = (all_preds == all_labels).mean() * 100
+                    for images, labels in train_loader:
+                        images, labels = images.to(device), labels.to(device)
 
-        cm = confusion_matrix(all_labels, all_preds)
+                        optimizer.zero_grad()
+                        outputs = model(images)
+                        loss = criterion(outputs, labels)
+                        loss.backward()
+                        optimizer.step()
+
+                        running_loss += loss.item()
+                        _, predicted = torch.max(outputs, 1)
+                        total += labels.size(0)
+                        correct += (predicted == labels).sum().item()
+
+                    train_loss = running_loss / len(train_loader)
+                    train_acc = correct / total * 100
+
+                    train_losses.append(train_loss)
+                    train_accs.append(train_acc)
+
+                    model.eval()
+                    running_loss = 0.0
+                    correct = 0
+                    total = 0
+
+                    with torch.no_grad():
+                        for images, labels in val_loader:
+                            images, labels = images.to(device), labels.to(device)
+
+                            outputs = model(images)
+                            loss = criterion(outputs, labels)
+
+                            running_loss += loss.item()
+                            _, predicted = torch.max(outputs, 1)
+                            total += labels.size(0)
+                            correct += (predicted == labels).sum().item()
+
+                            global_correct += (predicted == labels).sum().item()
+                            global_total += labels.size(0)
+
+                    val_loss = running_loss / len(val_loader)
+                    val_acc = correct / total * 100
+                    val_losses.append(val_loss)
+                    val_accs.append(val_acc)
+                    with open(OUTPUT_FILE, "a") as f:
+                        for line in [
+                            f"\r\nEpoch [{epoch+1}/{epochs}] | "
+                            f"Train Loss: {train_loss:.4f} | Train Acc: {train_acc:.2f}% | "
+                            f"Val Loss {val_loss:.4f} | Val Acc: {val_acc:.2f}%"
+                        ]:
+                            f.write(line)
+                    print(
+                        f"Epoch [{epoch+1}/{epochs}] | "
+                        f"Train Loss: {train_loss:.4f} | Train Acc: {train_acc:.2f}% | "
+                        f"Val Loss {val_loss:.4f} | Val Acc: {val_acc:.2f}%"
+                    )
+
+                    if not patience_trigger and val_acc > best_val_acc:
+                        best_val_acc = val_acc
+                        best_val_acc_epoch = epoch
+                        torch.save(model.state_dict(), "best_model.pt")
+
+                    if val_loss < best_val_loss:
+                        best_val_loss = val_loss
+                        epochs_no_improve = 0
+                    else:
+                        epochs_no_improve += 1
+
+                    if epochs_no_improve >= patience and not patience_trigger:
+                        patience_trigger = True
+                        with open(OUTPUT_FILE, "a") as f:
+                            f.write("\r\nEarly stopping triggered\n")
+                        print("Early stopping triggered")
+                        #break
+
+                model.load_state_dict(torch.load("best_model.pt", map_location=device))
+                model.eval()
+
+                all_preds = []
+                all_labels = []
+
+                with torch.no_grad():
+                    for images, labels in test_loader:
+                        images = images.to(device)
+                        labels = labels.to(device)
+
+                        outputs = model(images)
+                        _, predicted = torch.max(outputs, 1)
+
+                        all_preds.append(predicted.cpu())
+                        all_labels.append(labels.cpu())
+
+                all_preds = torch.cat(all_preds).numpy()
+                all_labels = torch.cat(all_labels).numpy()
+
+                test_accuracy = (all_preds == all_labels).mean() * 100
+
+                cm = confusion_matrix(
+                    all_labels,
+                    all_preds,
+                    labels=np.arange(len(class_codes))
+                )
 
 
-        global_accuracy = global_correct / global_total * 100
-        with open(OUTPUT_FILE, "a") as f:
-            for line in [
-                f"GLOBAL ACCURACY | LR={lr}, Momentum={momentum} | Accuracy: {global_accuracy:.2f}%",
-                f"TEST ACCURACY | LR={lr}, Momentum={momentum} | Accuracy: {test_accuracy:.2f}%",
-                "Confusion Matrix:",
-                str(cm),
-                "-"*50
-            ]:
-                print(line)
-                f.write(line + "\n")
-        #plt.figure(figsize=(6, 6))
-        #plt.imshow(cm)
-        #plt.title("Confusion Matrix")
-        #plt.xlabel("Predicted")
-        #plt.ylabel("True")
-        #plt.colorbar()
-        #plt.show()
-        
+                global_accuracy = global_correct / global_total * 100
+                with open(OUTPUT_FILE, "a") as f:
+                    for line in [
+                        f"\r\nGLOBAL ACCURACY | LR={lr}, Momentum={momentum} | Accuracy: {global_accuracy:.2f}%",
+                        f"TEST ACCURACY | LR={lr}, Momentum={momentum} | Accuracy: {test_accuracy:.2f}%",
+                        "Confusion Matrix:",
+                        str(cm),
+                        "-"*50
+                    ]:
+                        print(line)
+                        f.write(line + "\n")
 
-# plt.figure(figsize=(10, 4))
-# 
-# # Loss
-# plt.subplot(1, 2, 1)
-# plt.plot(train_losses, label='Train Loss')
-# plt.plot(val_losses, label='Val Loss')
-# plt.xlabel('Epoch')
-# plt.ylabel('Loss')
-# plt.title('Loss during training')
-# plt.legend()
-# plt.grid(True)
-# 
-# # Accuracy
-# plt.subplot(1, 2, 2)
-# plt.plot(train_accs, label='Train Accuracy')
-# plt.plot(val_accs, label='Val Accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy')
-# plt.title('Accuracy during training')
-# plt.legend()
-# plt.grid(True)
-# 
-# plt.tight_layout()
-# plt.show()
+
+                class_names = [label_map[c] for c in class_codes]
+                num_classes = len(class_names)
+
+                ticks = np.arange(num_classes)
+
+
+                fig, ax = plt.subplots(figsize=(12, 10))
+
+                sns.heatmap(
+                    cm,
+                    annot=True,
+                    fmt="d",
+                    cmap="Blues",
+                    cbar=True,
+                    xticklabels=class_names,
+                    yticklabels=class_names,
+                    linewidths=0.5,
+                    square=True,
+                    ax=ax
+                )
+
+                ax.set_title("Confusion Matrix")
+                ax.set_xlabel("Predicted label")
+                ax.set_ylabel("True label")
+
+                fig.tight_layout()
+                fig.savefig(
+                    f"confusion_matrix_v_03_results_lr_{lr}_m_{momentum}_e_{epochs}_p_{patience}.png",
+                    dpi=300,
+                    bbox_inches="tight"
+                )
+
+                plt.close(fig)
+
+                fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+
+                # Loss
+                sns.lineplot(
+                    x=np.arange(len(train_losses)),
+                    y=train_losses,
+                    ax=axes[0],
+                    label="Train Loss",
+                    marker="o"
+                )
+                sns.lineplot(
+                    x=np.arange(len(val_losses)),
+                    y=val_losses,
+                    ax=axes[0],
+                    label="Val Loss",
+                    marker="o"
+                )
+
+                axes[0].set_title("Loss during training")
+                axes[0].set_xlabel("Epoch")
+                axes[0].set_ylabel("Loss")
+                axes[0].legend()
+
+                # Accuracy
+                sns.lineplot(
+                    x=np.arange(len(train_accs)),
+                    y=train_accs,
+                    ax=axes[1],
+                    label="Train Accuracy",
+                    marker="o"
+                )
+                sns.lineplot(
+                    x=np.arange(len(val_accs)),
+                    y=val_accs,
+                    ax=axes[1],
+                    label="Val Accuracy",
+                    marker="o"
+                )
+
+                # Test accuracy point
+                axes[1].scatter(
+                    best_val_acc_epoch,
+                    test_accuracy,
+                    color="red",
+                    s=80,
+                    zorder=5,
+                    label="Test Accuracy"
+                )
+
+                axes[1].set_title("Accuracy during training")
+                axes[1].set_xlabel("Epoch")
+                axes[1].set_ylabel("Accuracy")
+                axes[1].legend()
+
+                fig.tight_layout()
+
+                fig.savefig(f"training_curves_v_03_results_lr_{lr}_m_{momentum}_e_{epochs}_p_{patience}.png", dpi=300, bbox_inches="tight")
+
+                plt.close(fig)
